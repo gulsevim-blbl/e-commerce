35214beb1d52b75fb787caa0dde75a3c9adb1388
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-03-19 17:22:40 +0300

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "../css/Header.css";
+import { SlBasket } from "react-icons/sl";
+import { IoSunny } from "react-icons/io5";
+import { FaMoon } from "react-icons/fa";
+
+const Header = () => {
+  const [theme, setTheme] = useState(false);
+
+  const changeTheme = () => {
+      const root = document.getElementById("root");
+      if(theme){
+          root.style.backgroundColor = "white";
+          root.style.color = "black";
+      }else{
+          root.style.backgroundColor = "black";
+          root.style.color = "white";
+      }
+      setTheme(!theme);
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <img className="logo" src="/Gsb-Logo.png" alt="GSB Logo" />
+      </div>
+      <div className="flex-row">
+        <input className="search-input" placeholder="Search" type="text" />
+        <div >
+          {
+               theme ? <FaMoon className="icon" onClick={changeTheme}/> : <IoSunny className="icon" onClick={changeTheme}/>
+          }
+          <SlBasket className="icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/container/PageContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from '@mui/material/Container';
+
+
+const PageContainer = ({children}) => {
+  return (
+    <Container maxWidth="lg">
+      {children}
+    </Container>
+  )
+}
+
+export default PageContainer
